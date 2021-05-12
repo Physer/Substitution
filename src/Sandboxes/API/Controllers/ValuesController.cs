99d59e40030d33a5beaac24b945af8f58b11306d
Alex Schouls <alex_schouls@live.com>
2021-05-12 19:13:55 +0200

    Added solution and project structure

---
+++ src/Sandboxes/API/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("Value");
+        }
+    }
+}
